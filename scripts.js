f1da42387c8841a28c1d89939db674108417996c
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-10-17 21:35:17 -0300

    feat: more sections, accordion

--- scripts.js
+++ scripts.js
@@ -23,7 +23,41 @@ const checkboxClicked = (event) => {
     : './assets/check-icon-mobile-checked.png';
 };
 
+function verifyActive(item, content, contentActived) {
+  const iconItem = item.querySelector('.icon');
+  const iconItemActive = document.querySelectorAll('.icon');
+
+  iconItemActive.forEach((item) => (item.innerHTML = '+'));
+
+  if (contentActived) {
+    contentActived.style.height = 0;
+    contentActived.classList.remove('active');
+  }
+
+  if (content !== contentActived) {
+    iconItem.innerHTML = '-';
+    content.classList.add('active');
+    content.style.height = content.scrollHeight + 10 + 'px';
+  }
+}
+
 //event listeners
 
 checkbox.addEventListener('click', (event) => checkboxClicked(event));
 checkbox2.addEventListener('click', (event) => checkboxClicked(event));
+
+// accordion
+
+const accordionItem = document.querySelectorAll('.accordion_item');
+
+accordionItem.forEach((item) => {
+  const accordionHeaderItem = item.querySelector('.accordion_header');
+
+  accordionHeaderItem.addEventListener('click', () => {
+    const accordionContentItem = item.querySelector('.accordion_content');
+
+    const itemActived = document.querySelector('.active');
+
+    verifyActive(item, accordionContentItem, itemActived);
+  });
+});
