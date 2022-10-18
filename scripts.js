40e0a32e42ab1daf130c00948b22d2d5fad9f9a2
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-10-18 16:18:12 -0300

    feat: boxes buttons

--- scripts.js
+++ scripts.js
@@ -3,6 +3,13 @@
 const nextSection = document.getElementById('dr_rafael__section');
 const checkbox = document.getElementById('checkbox');
 const checkbox2 = document.getElementById('checkbox2');
+const box1 = document.querySelectorAll('.box1');
+const box3 = document.querySelectorAll('.box3');
+const box6 = document.querySelectorAll('.box6');
+const avista = document.querySelectorAll('.avista');
+const aprazo = document.querySelectorAll('.aprazo');
+const avistaEconomize = document.querySelectorAll('.avista__economize');
+const aprazoEconomize = document.querySelectorAll('.aprazo__economize');
 
 //functions
 
@@ -11,7 +18,7 @@ const arrowClicked = () => {
 };
 
 const btnComprarClicked = () => {
-  alert('Item adicionado ao carrinho com sucesso! 😁');
+  alert('Adicionou ao carrinho com sucesso! 😁');
 };
 
 const checkboxClicked = (event) => {
@@ -61,3 +68,42 @@ accordionItem.forEach((item) => {
     verifyActive(item, accordionContentItem, itemActived);
   });
 });
+
+// handling number of boxes selection
+
+const setSelected = (numberOfBoxes) => {
+  const selecionados = document.querySelectorAll('.selected');
+
+  selecionados.forEach((item) => item.classList.remove('selected'));
+
+  switch (numberOfBoxes) {
+    case 1:
+      box1.forEach((item) => item.classList.add('selected'));
+      avista.forEach((item) => (item.innerText = 'R$194,00 por caixa'));
+      avistaEconomize.forEach((item) => (item.innerText = ''));
+      aprazo.forEach((item) => (item.innerText = 'R$21,75'));
+      aprazoEconomize.forEach((item) => (item.innerText = 'R$194,00 à vista'));
+      break;
+    case 3:
+      box3.forEach((item) => item.classList.add('selected'));
+      avista.forEach((item) => (item.innerText = 'R$148,60  por caixa'));
+      avistaEconomize.forEach((item) => (item.innerText = 'Economize R$136,20'));
+      aprazo.forEach((item) => (item.innerText = 'R$49,99'));
+      aprazoEconomize.forEach((item) => (item.innerText = 'R$445,80 à vista'));
+      break;
+    case 6:
+      box6.forEach((item) => item.classList.add('selected'));
+      avista.forEach((item) => (item.innerText = 'R$118,90 por caixa'));
+      avistaEconomize.forEach((item) => (item.innerText = 'Economize R$450,60'));
+      aprazo.forEach((item) => (item.innerText = 'R$79,99'));
+      aprazoEconomize.forEach((item) => (item.innerText = 'R$713,40 à vista'));
+      break;
+    default:
+      box3.forEach((item) => item.classList.add('selected'));
+      avista.forEach((item) => (item.innerText = 'R$148,60 por caixa'));
+      avistaEconomize.forEach((item) => (item.innerText = 'Economize R$136,20'));
+      aprazo.forEach((item) => (item.innerText = 'R$49,99'));
+      aprazoEconomize.forEach((item) => (item.innerText = 'R$445,80 à vista'));
+      break;
+  }
+};
